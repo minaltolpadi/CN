a040cf70d64738414a843c75e0e5906addfd574a
Minal <minaltolpadi@gmail.com>
2016-03-23 15:39:56 -0400

    topology file

---
+++ project_topology_creation.py
@@ -0,0 +1,25 @@
+from mininet.topo import Topo
+
+class MyTopo(Topo):
+	"Project topology."
+
+	def __init__(self):
+		"Create topology for the project."
+
+		#Initialize topology
+		Topo.__init__(self)
+
+		#Add hosts and switches
+		leftHost = self.addHost('h1')
+		rightHost = self.addHost('h2')
+		upperSwitch = self.addSwitch('s3')
+		lowerSwitch = self.addSwitch('s4')
+
+		#Add links
+		self.addLink(lefthost,upperSwitch)
+		self.addLink(lefthost,lowerSwitch)
+		self.addLink(righthost,upperSwitch)
+		self.addLink(righthost,lowerSwitch)
+
+
+topos = {'mytopo':(lambda:MyTopo())}
